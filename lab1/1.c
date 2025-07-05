babb1106c572bee54ec38ae493c66d2b5289a67e
waBenford <thanakorngithub@gmail.com>
2025-07-05 12:59:32 +0700

    edit1

--- lab1/1.c
+++ lab1/1.c
@@ -10,7 +10,7 @@ int main() {
     scanf("%d", &Age);  // อ่านอายุ (ต้องใช้ &)
 
     printf("- - - - - -\n");
-    printf("Hello %s\n", Name);  // แสดงชื่อ
+    printf("Kuy %s\n", Name);  // แสดงชื่อ
     printf("Age = %d\n", Age);   // แสดงอายุ
 
     return 0;
