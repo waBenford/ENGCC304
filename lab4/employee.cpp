4e0adc4607fd5d46215fb37817c331e14e5b600b
waBenford <thanakorngithub@gmail.com>
2025-07-24 14:59:21 +0700

    finish

---
+++ lab4/employee.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int numbers;
+    int hrs;
+    float salary;
+
+    printf("Input the Employees ID(Max. 10 chars): \n");
+    scanf("%d", &numbers);
+    printf("Input the working hrs: \n");
+    scanf("%d", &hrs);
+    printf("Salary amount/hr: \n");
+    scanf("%f", &salary);
+
+    printf("Expected Output: \n");
+    printf("Employees ID = %d\n", numbers);
+    printf("Salary = U$ %.2f\n", hrs * salary);
+}
