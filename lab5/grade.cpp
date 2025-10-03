7403f4212404f2427b84530b021e143cb1d29b7c
waBenford <thanakorngithub@gmail.com>
2025-10-03 14:50:28 +0700

    Update last

--- lab5/grade.cpp
+++ lab5/grade.cpp
@@ -3,26 +3,29 @@
 int main() {
     int score;
 
-    printf( "Enter your score: \n" );
-    scanf( "%d" , &score );
-
-    if ( score >= 80 ) {
-        printf( "A !" );
-    } else if( score >= 75 ) {
-        printf( "B+ !" );
-    } else if( score >= 70 ) {
-        printf( "B !" );
-    } else if( score >= 65 ) {
-        printf( "C+ !" );
-    } else if( score >= 60 ) {
-        printf( "C !" );
-    } else if( score >= 55 ) {
-        printf( "D+ !" );
-    } else if( score >= 50 ) {
-        printf( "D !" );
-    } else {
-        printf( "F !" );
+    printf("Enter your score: \n");
+    if (scanf("%d", &score) != 1) {
+        printf("please enter number only.\n");
+        return 1;
     }
-    
+
+    if (score >= 80) {
+        printf("A !\n");
+    } else if (score >= 75) {
+        printf("B+ !\n");
+    } else if (score >= 70) {
+        printf("B !\n");
+    } else if (score >= 65) {
+        printf("C+ !\n");
+    } else if (score >= 60) {
+        printf("C !\n");
+    } else if (score >= 55) {
+        printf("D+ !\n");
+    } else if (score >= 50) {
+        printf("D !\n");
+    } else {
+        printf("F !\n");   
+    }
+
     return 0;
 }
