c09d513268a5c4cb473fd8ee058ce5e24a5b72e1
waBenford <thanakorngithub@gmail.com>
2025-10-03 15:08:25 +0700

    update for send work last term

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -5,7 +5,7 @@ int main() {
     int i;
 
     while (1) {
-        printf( "input your num:" );
+        printf( "Enter value: \n" );
         if (scanf( "%d",&n ) == 1) {
         break;
         }else {
@@ -13,17 +13,19 @@ int main() {
         }
     }//end while
 
+    printf("Series:");
+    
     if (n % 2 == 1) {
         for (i = 1 ; i <= n ; i++) {
             if (i % 2 == 1) {
-            printf( "%d ",i );
+            printf( " %d ",i );
             }
 
         }
     }else {
         for (i = n ; i >= 0 ; i--) {
             if (i % 2 == 0) {
-            printf( "%d ",i );
+            printf( " %d ",i );
             }
         }
     }
